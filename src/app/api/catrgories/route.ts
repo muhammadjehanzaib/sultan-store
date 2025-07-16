af74cda082f1906d1b76a0dc8bb81fc6181fb162
Muhammad Jehanzaib <engmuhammadjehanzaib@gmail.com>
2025-07-16 12:36:21 +0500

    Prisma scheme updated

--- src/app/api/catrgories/route.ts
+++ src/app/api/catrgories/route.ts
@@ -1,5 +1,5 @@
 // import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from '@prisma/client';
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
